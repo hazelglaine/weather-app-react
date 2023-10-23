58066f8089315f971ca44c4ed6c86c1348fd5ecb
hazelglaine <hazelglainesales@gmail.com>
2023-10-23 22:38:01 +0800

    Added Footer.js component

---
+++ src/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <p>
+        This project is coded by Hazel and{" "}
+        <a
+          href="https://github.com/hazelglaine/weather-app-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>
+        .
+      </p>
+    </div>
+  );
+}
